24178f7081fd6c1d5806c553764489d96330c14b
N1TROGUE <n1trogue@protonmail.com>
2025-02-28 11:09:41 +0100

    Created factory for the CommunityNight model

---
+++ app/Models/CommunityNight.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommunityNight extends Model
+{
+    use HasFactory;
+}
