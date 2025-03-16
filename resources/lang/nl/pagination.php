7ab57635593575b25c7039945beeca79a3a7bd4f
Just <167076399+jbhuisman@users.noreply.github.com>
2025-03-16 17:27:17 +0100

    Revert "Merge branch 'Dev' into 68-Navigationbar_Desktop"
    This reverts commit d2017544b6d6259f967304cc29ea9a1095e0d305, reversing
    changes made to 4be1c688b0816927554e5767b1214a6b39a3851e.

--- resources/lang/nl/pagination.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'previous' => '&laquo; Vorige',
-    'next' => 'Volgende &raquo;',
-    'showing' => 'Toont :first tot :last van :total resultaten',
-];
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 // routes/web.php
-Route::get('/', function () {
-    return redirect('/create_evenement');
+Route::get('/example', function () {
+    return view('example');
 });
-
-Route::get('/create_evenement', [EvenementenController::class, 'create']);
-Route::post('/create_evenement', [EvenementenController::class, 'store']);
-Route::get('/index_evenement', [EvenementenController::class, 'index']);
