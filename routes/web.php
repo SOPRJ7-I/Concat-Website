9e7b6c9c509e501e6ef5035d5a3d1c9b4e02078e
N1TROGUE <n1trogue@protonmail.com>
2025-03-17 00:38:55 +0100

    Fixed pagination not showing on mobile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommunityNightController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EvenementenController;
 // routes/web.php
