f474379de667a4266fa74e938342454bebbaf48b
N1TROGUE <n1trogue@protonmail.com>
2025-05-26 22:09:49 +0200

    Improves event card layout and data display
    Enhances the layout of event cards on the homepage to improve user experience.
    
    - Adjusts the image display to maintain aspect ratio
    - Adds more space for event details and description
    - Fixes styling issues for consistency

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,27 +6,31 @@
                 <div class="w-full lg:max-w-6/12 flex flex-col h-full">
                     <h1 class="text-3xl font-bold mb-2 text-left">Evenementen</h1>
                     <hr class="border-b-4 border-purple-500 mb-4">
-                    <div class="grid gap-8 lg:gap-6">
+                    <div class="grid gap-8 lg:gap-6 flex-grow">
                         <div class="bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden flex flex-col h-full">
                             <a href="{{ route('evenementen.show', $event->id) }}"
+                               class="block w-full aspect-square relative overflow-hidden"
                                aria-label="Details bekijken van {{ $event->titel }}">
                                 @if(isset($event->afbeelding) && isset($event->start_datum) && isset($event->einddatum) && isset($event->locatie))
                                     <img src="{{ $event->afbeelding }}"
                                          alt="Afbeelding van {{ $event->titel }}. Datum: {{ \Carbon\Carbon::parse($event->start_datum)->format('d-m-Y') }} tot {{ \Carbon\Carbon::parse($event->einddatum)->format('d-m-Y') }} in {{ $event->locatie }}"
                                          class="w-full object-cover">
                                 @else
-                                    <div class="p-5 sm:h-44 flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-500" aria-hidden="true">
+                                    <div class="p-5 flex h-full w-fill items-center justify-center bg-gradient-to-r from-blue-400 to-purple-500" aria-hidden="true">
                                         <h1 class="text-white text-3xl font-bold text-center w-full break-words">{{ $event->titel }}</h1>
                                     </div>
                                 @endif
                             </a>
 
-                            <div class="p-7">
+                            <div class="p-5 flex flex-col flex-grow">
+                                {{-- Titel --}}
                                 {{-- Categorie --}}
                                 @if(isset($event->categorie))
-                                    <span class="inline-block mb-2 bg-purple-100 text-purple-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+                                    <div>
+                                        <span class="inline-block mb-2 bg-purple-100 text-purple-700 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
                                             {{ ucfirst($event->categorie) }}
-                                </span>
+                                        </span>
+                                    </div>
                                 @endif
 
                                 {{-- Datum & tijd --}}
@@ -56,34 +60,36 @@
                                     <div class="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-b from-transparent to-white" aria-hidden="true"></div>
                                 </div>
 
-                                <div class="flex items-center text-gray-500 mb-4">
-                                    <i class="flex-shrink-0 fa-solid fa-users text-3xl" alt="Aantal inschrijvingen evenement" aria-hidden="true"></i>
-                                    <div class="ml-2">
-                                        <!-- Totaal aantal plekken -->
-                                        <div class="text-lg font-bold">
-                                            Totaal aantal plekken:
-                                            @if($availableSpots > 0)
-                                                {{ $availableSpots }} plekken
-                                            @else
-                                                Geen plaatsen beschikbaar
+                                <div class="mt-auto">
+                                    <div class="flex items-center text-gray-500 mb-4">
+                                        <i class="flex-shrink-0 fa-solid fa-users text-3xl" alt="Aantal inschrijvingen evenement" aria-hidden="true"></i>
+                                        <div class="ml-2">
+                                            <!-- Totaal aantal plekken -->
+                                            <div class="text-lg font-bold">
+                                                Totaal aantal plekken:
+                                                @if($availableSpots > 0)
+                                                    {{ $availableSpots }} plekken
+                                                @else
+                                                    Geen plaatsen beschikbaar
+                                                @endif
+                                            </div>
+
+                                            <!-- Aantal ingeschreven -->
+                                            @if(auth()->user() && auth()->user()->is_admin) <!-- Check if the user is admin -->
+                                            <div class="text-lg font-bold">
+                                                Aantal ingeschreven: {{ $registeredCount }}
+                                            </div>
                                             @endif
                                         </div>
-
-                                        <!-- Aantal ingeschreven -->
-                                        @if(auth()->user() && auth()->user()->is_admin) <!-- Check if the user is admin -->
-                                        <div class="text-lg font-bold">
-                                            Aantal ingeschreven: {{ $registeredCount }}
-                                        </div>
-                                        @endif
                                     </div>
-                                </div>
 
-                                {{-- Lees meer knop --}}
-                                <a href="{{ route('evenementen.show', $event->id) }}"
-                                   class="inline-flex items-center text-sm text-center bg-[#3129FF] text-white py-2 px-4 rounded-lg hover:bg-[#E39FF6] transition font-semibold"
-                                   aria-label="Lees meer over {{ $event->titel }}. Datum van {{ \Carbon\Carbon::parse($event->start_datum)->format('d-m-Y') }} tot {{ \Carbon\Carbon::parse($event->einddatum)->format('d-m-Y') }} in {{ $event->locatie }}">
-                                    Lees meer over {{ $event->titel }}
-                                </a>
+                                    {{-- Lees meer knop --}}
+                                    <a href="{{ route('evenementen.show', $event->id) }}"
+                                       class="inline-flex items-center text-sm text-center bg-[#3129FF] text-white py-2 px-4 rounded-lg hover:bg-[#E39FF6] transition font-semibold"
+                                       aria-label="Lees meer over {{ $event->titel }}. Datum van {{ \Carbon\Carbon::parse($event->start_datum)->format('d-m-Y') }} tot {{ \Carbon\Carbon::parse($event->einddatum)->format('d-m-Y') }} in {{ $event->locatie }}">
+                                        Lees meer over {{ $event->titel }}
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -104,7 +110,7 @@
                             @endif
                         </a>
 
-                        <div class="p-5">
+                        <div class="p-5 flex flex-col flex-grow">
                             {{-- Temporarily disabled, breaks at times for unknown reasons --}}
                             @if(isset($communityNight->image))
                                 <a href="{{ route('community-nights.show', $communityNight) }}">
@@ -126,13 +132,12 @@
                                 </div>
                             @endif
 
-
-                            <div class="mb-4 grow text-gray-700 relative overflow-hidden max-h-32">
-                                <p class="mb-3 font-normal text-gray-700 ">{{ $communityNight->description }}</p>
-                                <div class="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-b from-transparent to-white"></div>
-                            </div>
-
                             <div class="mt-auto"> {{-- Pushes button to bottom --}}
+                                <div class="mb-4 grow text-gray-700 relative overflow-hidden">
+                                    <p class="mb-3 font-normal text-gray-700 ">{{ $communityNight->description }}</p>
+                                    <div class="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-b from-transparent to-white"></div>
+                                </div>
+
                                 <a href="{{ route('community-nights.show', $communityNight) }}" class="inline-flex items-center text-sm text-center bg-[#3129FF] text-white py-2 px-4 rounded-lg hover:bg-[#E39FF6] transition font-semibold">
                                     Lees meer...
                                 </a>
