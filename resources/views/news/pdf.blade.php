67d2c26c434d399f86ed18ea9add8f3f85b9128c
Luuk <l.dekramer@student.avans.nl>
2025-05-31 15:30:19 +0200

    css verplaatst

--- resources/views/news/pdf.blade.php
+++ resources/views/news/pdf.blade.php
@@ -3,40 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
-    <style>
-        body {
-            font-family: sans-serif;
-            line-height: 1.5;
-            padding: 30px;
-        }
-
-        h1 {
-            text-align: center;
-            margin-bottom: 40px;
-        }
-
-        .content {
-            white-space: pre-line;
-            margin-bottom: 40px;
-        }
-
-        .image-container {
-            text-align: center;
-            margin-bottom: 30px;
-        }
-
-        img {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
+    @Vite('resources/css/app.css')
 </head>
 
 <body>
     <h1>{{ $title }}</h1>
 
     <div class="content">{!! $content !!}</div>
-    
+
     {{-- Afbeeldingen tonen --}}
     @if(!empty($images))
         @foreach ($images as $image)
